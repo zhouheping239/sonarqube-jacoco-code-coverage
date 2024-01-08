df85a4a3903f998853a447674f3a69fb46ca4d5c
zhouheping239 <88753904+zhouheping239@users.noreply.github.com>
2024-01-08 10:34:34 +0000

    add more smell code to MathService.java

--- src/main/java/com/tom/service/MathService.java
+++ src/main/java/com/tom/service/MathService.java
@@ -9,6 +9,16 @@ public class MathService {
     public int subtract(int firstNumber, int secondNumber) {
         Integer myInt = null;
         Integer yourInt = myInt * 2;
+        Integer myInt1 = null;
+        Integer myInt2 = null;
+        Integer myInt3 = null;
+        Integer myInt4 = null;
+        Integer myInt5 = null;
+        Integer yourInt1 = myInt1 * 2;
+        Integer yourInt2 = myInt2 * 2;
+        Integer yourInt3 = myInt3 * 2;
+        Integer yourInt4 = myInt4 * 2;
+        Integer yourInt5 = myInt5 * 2;
         return firstNumber - secondNumber;
     }
 }
